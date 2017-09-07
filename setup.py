ac07fcff1ad8c2693bc2aeb491cd537c665df64a
Evan Liu <hmisty@gmail.com>
2017-09-07 19:22:41 +0800

    the first setup

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+# MIT License
+#
+# Copyright (c) 2017 Evan Liu (hmisty)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+from distutils.core import setup
+
+VERSION = "1.1.1"
+URLBASE = "https://github.com/hmisty/bson-rpc/"
+URLMAP = {
+		"daily": "tarball/master",
+		"1.1": "tarball/1.1",
+		}
+
+if __name__ == "__main__":
+	setup(
+			name='bson-rpc',
+			version=VERSION,
+			description='a lightweight, high performance, multilingual RPC library',
+			author='Evan Liu (hmisty)',
+			author_email='hmisty@gmail.com',
+			url=URLBASE,
+			download_url='/'.join([URLBASE, URLMAP.get(VERSION, URLMAP['daily'])]),
+			packages=[
+				'bson-rpc'
+				],
+			scripts=[],
+			license= 'MIT',
+			keywords = ['BSON', 'bson-rpc', 'rpc'],
+			classifiers = [
+				'Development Status :: 4 - Beta',
+				'Environment :: Web Environment',
+				'Framework :: Gevent',
+				'Intended Audience :: Developers',
+				'License :: OSI Approved :: MIT License',
+				'Operating System :: OS Independent',
+				'Programming Language :: Python :: 2.6',
+				'Programming Language :: Python :: 2.7',
+				'Topic :: Software Development :: Libraries :: Python Modules',
+				]
+			)
