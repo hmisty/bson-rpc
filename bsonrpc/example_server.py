a315175d1e60b5a36ca4b81d00b279f265debee8
Evan Liu <hmisty@gmail.com>
2017-09-07 20:37:30 +0800

    rpc & rpc_func

---
+++ bsonrpc/example_server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+# MIT License
+#
+# Copyright (c) 2017 Evan Liu (hmisty)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+
+from bson-rpc import rpc, rpc_func
+
+@rpc_func
+def echo(s):
+    return s
+
+@rpc_func
+def add(a, b):
+    return a + b
+
+host = '127.0.0.1'
+port = 8181
+server = rpc.server(host, port)
+server.start()
+
