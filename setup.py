f4252d82ec575d995c88ba80b51270413c284a76
Evan Liu <hmisty@gmail.com>
2017-09-13 15:19:10 +0800

    remove Framework / Py 2.6 classifier

--- setup.py
+++ setup.py
@@ -50,11 +50,9 @@ if __name__ == "__main__":
 			classifiers = [
 				'Development Status :: 4 - Beta',
 				'Environment :: Web Environment',
-				'Framework :: Gevent',
 				'Intended Audience :: Developers',
 				'License :: OSI Approved :: MIT License',
 				'Operating System :: OS Independent',
-				'Programming Language :: Python :: 2.6',
 				'Programming Language :: Python :: 2.7',
 				'Topic :: Software Development :: Libraries :: Python Modules',
 				]
