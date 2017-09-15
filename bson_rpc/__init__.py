a30bdad6ac382db43380aaecd2ab805dff8b90c4
Evan Liu <hmisty@gmail.com>
2017-09-15 16:21:31 +0800

    back to direct socket version

--- bson_rpc/__init__.py
+++ bson_rpc/__init__.py
@@ -1,16 +1,16 @@
-__version = (0, 5, 2)
+__version = (0, 6, 0)
 
 __version__ = version = '.'.join(map(str, __version))
 __project__ = PROJECT = __name__
 
 #__all__ = ['rpc', 'start_server']
 
-import server_zmq
-rpc = server_zmq.rpc
-start_server = server_zmq.start
+import server
+rpc = server.rpc
+start_server = server.start
 
-import client_zmq
-connect = client_zmq.connect
+import client
+connect = client.connect
 
 #import sys
 #defaultencoding = 'utf-8'
